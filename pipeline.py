905e3d6dfae3f117422e6b9d6dd36604d7e49c5b
gabrocecco <gabrocecco@gmail.com>
2024-02-09 12:39:32 +0100

    chessboard_piece_assign

--- pipeline.py
+++ pipeline.py
@@ -4,6 +4,7 @@ from IPython.display import display
 import os
 import cv2
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 import numpy as np
 
 # order points in this way:
@@ -23,13 +24,104 @@ def order_points(pts):
     
     return rect
 
+
+def plot_grid_on_transformed_image(image):
+    
+    corners = np.array([[0,0], 
+                    [image.size[0], 0], 
+                    [0, image.size[1]], 
+                    [image.size[0], image.size[1]]])
+    
+    corners = order_points(corners)
+
+    figure(figsize=(10, 10), dpi=80)
+
+    # im = plt.imread(image)
+    implot = plt.imshow(image)
+    
+    TL = corners[0]
+    BL = corners[3]
+    TR = corners[1]
+    BR = corners[2]
+
+    def interpolate(xy0, xy1):
+        x0,y0 = xy0
+        x1,y1 = xy1
+        dx = (x1-x0) / 8
+        dy = (y1-y0) / 8
+        pts = [(x0+i*dx,y0+i*dy) for i in range(9)]
+        return pts
+
+    ptsT = interpolate( TL, TR )
+    ptsL = interpolate( TL, BL )
+    ptsR = interpolate( TR, BR )
+    ptsB = interpolate( BL, BR )
+        
+    for a,b in zip(ptsL, ptsR):
+        plt.plot( [a[0], b[0]], [a[1], b[1]], 'ro', linestyle="--" )
+    for a,b in zip(ptsT, ptsB):
+        plt.plot( [a[0], b[0]], [a[1], b[1]], 'ro', linestyle="--" )
+        
+    plt.axis('off')
+
+    plt.savefig('chessboard_transformed_with_grid.jpg')
+    return ptsT, ptsL
+
+
+# connects detected piece to the right square
+
+def connect_square_to_detection(points, ptsT, ptsL):
+    
+    # di = {0: 'b', 1: 'k', 2: 'n',
+    #   3: 'p', 4: 'q', 5: 'r', 
+    #   6: 'B', 7: 'K', 8: 'N',
+    #   9: 'P', 10: 'Q', 11: 'R'}
+    
+    # print("ciao")
+    # print(ptsT)
+    # print("\n")
+    # print(ptsL)
+    square_list_centers = []
+    for t in range(1,len(ptsT)):
+        for l in range(1,len(ptsL)):
+            pTt = ptsT[t]
+            pTt = pTt[0]
+
+            pTt_1 = ptsT[t-1]
+            pTt_1 = pTt_1[0]
+
+            pLl = ptsL[l]
+            pLl = pLl[1]
+
+            pLl_1 = ptsL[l-1]
+            pLl_1 = pLl_1[1]
+
+            x = pTt - pTt_1
+            y = pLl - pLl_1
+
+
+            # x =  ptsT[t][0] - ptsT[t-1][0]
+            
+            # y =  ptsL[l][1] - ptsL[l-1][1]
+
+            # print(np.array(ptsT[t])[0])
+            # print(np.array(ptsT[t])[1])
+            p = [x,y]
+            print(t)
+            print(l)
+            print("\n")
+            print(p)
+            square_list_centers.append(p)
+    return 
+
+
 # Orginal phote taken by the app
 #starting_image_link = "dataset\\train\images\\04aed88a8d23cf27e47806eb23948495_jpg.rf.b2b9c08d458461669627c4976b744f46.jpg"
 starting_image_link = "test_images\\test_images_real\IMG-20240208-WA0003.jpg"
 
 #detect pieces with otiginal yolo model 
 model_pieces = YOLO("training_output\content\\runs\detect\\train2_200_epocs\weights\\best.pt")
-results = model_pieces.predict(starting_image_link, save=True, show=False, project="yolo_output_final_warped", name="on_original_perspective", exist_ok=True)
+results_pieces_original = model_pieces.predict(starting_image_link, save=True, iou=0.2, show=False, project="yolo_output_final_warped", name="on_original_perspective", exist_ok=True)
 
 #chessboard corner detection 
 model_corner = YOLO("best_corners_repo.pt")
@@ -95,12 +187,62 @@ print("TRANSFORM MATRIX: \n"+str(M)+"\n\n")
 img = Image.open(starting_image_link)
 image = np.asarray(img)
 warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-img = Image.fromarray(warped, "RGB")
-img.save("p_test.jpg")
+img_warped = Image.fromarray(warped, "RGB")
+img_warped.save("p_test.jpg")
 # img.show()
 
 
-conf_par = 0.1
-results = model_pieces.predict("p_test.jpg", save=True, show=False, project="yolo_output_final_warped",name="on_warped_perspective", exist_ok=True)
+
+boxes = results_pieces_original[0].boxes
+arr = boxes.xywh.numpy()
+points = arr[:,0:2]
+print("POINTS_DETECTS_ORIGINAL: \n"+ str(points)+"\n\n")
+points = np.float32(np.array(points))
+print("POINTS_DETECTS_ORIGINAL: \n"+ str(points)+"\n\n")
+
+list_point_detetcts = []
+
+for point in points:
+    p = np.float32(np.array([[point]]))
+    new_point = cv2.perspectiveTransform(p, M)
+    new_point = new_point[0][0]
+    new_point[1] = new_point[1] + 40
+    print(new_point)
+    list_point_detetcts.append(new_point)
+
+print(len(list_point_detetcts))
+np.size(list_point_detetcts)
+
+
+img = plt.imread("p_test.jpg")
+index = 0
+for point in list_point_detetcts:
+    plt.scatter(point[0], point[1])
+    index = index + 1
+plt.imshow(img)
+plt.show()
+
+
+
+ptsT, ptsL = plot_grid_on_transformed_image(img_warped)
+
+print(ptsT)
+print(ptsL)
+
+img = plt.imread("chessboard_transformed_with_grid.jpg")
+# img = plt.imread("Figure_1.png")
+index = 0
+for point in list_point_detetcts:
+    plt.scatter(point[0], point[1])
+    index = index + 1
+plt.imshow(img)
+plt.show()
+
+
+
+
+connect_square_to_detection(list_point_detetcts, ptsT, ptsL)
+#conf_par = 0.1
+#results = model_pieces.predict("p_test.jpg", save=True, show=False, project="yolo_output_final_warped",name="on_warped_perspective", exist_ok=True)
 
 
