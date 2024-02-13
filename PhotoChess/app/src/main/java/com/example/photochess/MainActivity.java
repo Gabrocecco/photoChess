c8057ebf2c4bc148be4c1b5d51b5cc807eec644e
Rocco Pastore <roccopastore@MacBook-Air-di-Rocco.local>
2024-02-13 08:57:50 +0100

    updated main activity

--- PhotoChess/app/src/main/java/com/example/photochess/MainActivity.java
+++ PhotoChess/app/src/main/java/com/example/photochess/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.photochess;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -41,6 +44,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ImageView howBtn = findViewById(R.id.howButton);
+        howBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                howtotakephoto();
+            }
+        });
+
+    }
+
+    public void howtotakephoto(){
+       View howlayout = getLayoutInflater().inflate(R.layout.how_dialog, null, false);
+       AlertDialog.Builder builder = new AlertDialog.Builder(this);
+       builder.setView(howlayout);
+       builder.setTitle("How to take the photo");
+       builder.setIcon(R.drawable.blackrook);
+       builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+           public void onClick(DialogInterface dialog, int which) {
+           }});
+       AlertDialog alert = builder.create();
+       alert.show();
+       alert.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.mygreen));
     }
 
 }
